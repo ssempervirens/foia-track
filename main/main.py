32924b1c480d1b199c2d3ffede0ecf6e8eb9fb43
Camille Fassett <camillefassett@gmail.com>
2020-06-16 18:00:30 -0700

    correct paths

--- main/main.py
+++ main/main.py
@@ -8,7 +8,7 @@ from new_request import new_request_row
 # every day, run this script and read in csv, and if it has been one week since
 # last check in
 
-tracker_sheet_file = pathlib.Path("../tracker-sheet/foia_track_sheet.csv")
+tracker_sheet_file = pathlib.Path("tracker-sheet/foia_track_sheet.csv")
 
 initial_column_names = ['agency', 'date_requested', 'tracking_num', 'request_subject', 'followup_interval', 'required_response_time']
 # might want to add custom columns like contact at agency, state of agency like CA, OR, etc]
@@ -22,7 +22,7 @@ def setup():
         tracker = pd.DataFrame(columns = initial_column_names)
     if tracker_sheet_file.exists():
         print("It looks like you aready have a tracker spreadsheet. Reading it in...")
-        tracker = pd.read_csv('../tracker-sheet/foia_track_sheet.csv')
+        tracker = pd.read_csv('tracker-sheet/foia_track_sheet.csv')
     return tracker
 
 
@@ -32,4 +32,4 @@ if __name__ == '__main__':
     new_row = new_request_row()
     tracker = tracker.append(new_row, ignore_index=True)
     print(tracker)
-    tracker.to_csv('../tracker-sheet/foia_track_sheet.csv', index=False)
+    tracker.to_csv('tracker-sheet/foia_track_sheet.csv', index=False)
