16ecbfa4b5701a25afa5fdeb72268b9b0e49c0e1
Camille Fassett <camillefassett@gmail.com>
2022-03-07 19:21:32 -0800

    start functions for getting today's requests

--- main/track_requests.py
+++ main/track_requests.py
@@ -1,5 +1,16 @@
 # get today's requests and print/format them nicely
+from datetime import date
 
-def track_requests(df):
-    df.to_sql('requests', conn, if_exists='append', index = False)
+def track_requests(cursor):
+    # decrease days_to_followup by one each time script is run
+    cursor.execute(
+        "UPDATE requests SET days_to_followup = days_to_followup - 1 WHERE days_to_followup > 0"
+    )
     pass
+
+def get_today_requests(cursor):
+    cursor.execute(
+        "SELECT * FROM requests WHERE days_to_followup=0"
+    )
+
+# each time script is run, decrease days_to_followup by one? or calculate if days since date requested is divisible by followup interval?
