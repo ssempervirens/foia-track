9042cc74e07189125634d137cbdb09274041fd9b
Camille Fassett <camillefassett@gmail.com>
2022-03-07 19:20:54 -0800

    clean up main file and add column values

--- main/main.py
+++ main/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import datetime as datetime
+import datetime
 import pathlib
 import os
 from os import path
@@ -7,13 +7,9 @@ from new_request import new_request_row
 from track_requests import track_requests
 import sqlite3
 
-# every day, run this script and read in csv, and if it has been one week since
-# last check in
 
-tracker_sheet_file = pathlib.Path("tracker-sheet/foia_track_sheet.csv")
-
-initial_column_names = ['agency', 'date_requested', 'tracking_num', 'request_subject', 'followup_interval', 'required_response_time']
 # might want to add custom columns like contact at agency, state of agency like CA, OR, etc]
+
 
 def determine_track(cursor):
     """ Determine whether to create a new request or track existing requests """
@@ -24,11 +20,11 @@ def determine_track(cursor):
         print('true')
         print("Entering add new request mode... \n")
         new_row = new_request_row()
-        cursor.execute("insert into requests (requesttitle, agency, date_requested, tracking_num, followup_interval, required_response_time, request_description) values (?, ?, ?, ?, ?, ?, ?)", new_row)
+        cursor.execute("insert into requests (requesttitle, agency, date_requested, tracking_num, followup_interval, required_response_time, days_to_followup, request_description) values (?, ?, ?, ?, ?, ?, ?, ?)", new_row)
 
 
 if __name__ == '__main__':
     connection = sqlite3.connect("foiatrack.db")
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS requests (requesttitle TEXT, agency TEXT, date_requested TEXT, tracking_num TEXT, followup_interval INTEGER, required_response_time INTEGER, request_description TEXT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS requests (requesttitle TEXT, agency TEXT, date_requested TEXT, tracking_num TEXT, followup_interval INTEGER, required_response_time INTEGER, days_to_followup INTEGER, request_description TEXT)")
     determine_track(cursor)
